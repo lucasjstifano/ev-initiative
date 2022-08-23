cd31301efb0bc675fb1d30a463903a006927be85
lucasjstifano <lucasjstifano@gmail.com>
2022-08-23 19:53:39 -0400

    Cleaned data.js file x2

--- src/data.js
+++ src/data.js
@@ -141,25 +141,6 @@ export const timelineInfo = [
   },
 ];
 
-export const FQAInfo = [
-  {
-    text: "What are the objectives of this token?",
-    ID: "Q1",
-  },
-  {
-    text: "What is Token Sale and pre-sale?",
-    ID: "Q2",
-  },
-  {
-    text: "What is the pre-sale start date?",
-    ID: "Q3",
-  },
-  {
-    text: "how may I take part in pre-sale?",
-    ID: "Q4",
-  },
-];
-
 export const VerticalSocial = [
   // {
   //   nameIco: "fa-brands fa-telegram",
